7d7944abead7126ae945884e1a0a55279c949532
Jimmy Ho <ho@jimmyho.com>
2015-12-01 01:41:15 +0000

    fixed plugins

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,23 @@
 var path = require('path'),
-    libPath = path.join(__dirname, 'lib'),
-    pkg = require('./package.json'),
-    webpack = require("webpack"),
-    extend = require('util')._extend,
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    webpackConfig = require('./webpack.config.js');
+  libPath = path.join(__dirname, 'lib'),
+  pkg = require('./package.json'),
+  webpack = require("webpack"),
+  extend = require('util')._extend,
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  webpackConfig = require('./webpack.config.js');
 
 module.exports = extend(webpackConfig, {
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            pkg: pkg,
-            template: path.join(libPath, 'index.html')
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin()
-    ]
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      pkg: pkg,
+      template: path.join(libPath, 'index.html')
+    }),
+    new webpack.ProvidePlugin({
+      register: 'registerjs'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 });
